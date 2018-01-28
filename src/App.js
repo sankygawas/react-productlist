0033952e7a2f70d571b6c2a50b009f7bafd0274e
Sanket Arun Gawas <sanketgawas1993@gmail.com>
2018-01-28 20:36:47 +0530

    Implement Search

--- src/App.js
+++ src/App.js
@@ -5,14 +5,28 @@ import ProductsTable from "./components/ProductsTable";
 import SearchBar from "./components/SearchBar";
 
 class FilterableProductTable extends Component {
+  constructor(props){
+    super();
+    this.state = { 
+      updatedList:props.products 
+    };
+  }
+  onHandleSearch = (searchValue)=>{
+    let filteredList = this.props.products.filter((item)=>{
+        return item.name.toLowerCase().search(searchValue.toLowerCase()) !== -1;
+    })
+    this.setState({ updatedList: filteredList});
+  }
+
   render() {
+
     return <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-          <SearchBar />
+          <SearchBar handleSearch={this.onHandleSearch} />
         </header>
-        <ProductsTable products={this.props.products} />
+        <ProductsTable products={this.state.updatedList} />
       </div>;
   }
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
 
 class SearchBar extends Component{
+
+    onHandleSearch(event){
+        this.props.handleSearch(event.target.value);
+    }
     render(){
-        return <input className="form-control w-25 mx-auto" placeholder="Search" />;
+        return <input className="form-control w-25 mx-auto" placeholder="Search" onChange={this.onHandleSearch.bind(this)} />;
     }
 }
 
